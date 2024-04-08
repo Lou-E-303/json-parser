d69a8f1a66bd74acc3605b5c09276b966a913b62
Lou-E-303 <louiscowell404@gmail.com>
2024-04-08 20:12:04 +0100

    Added ArgumentParser and Token enum

---
+++ src/main/java/constants/Token.java
@@ -0,0 +1,9 @@
+package constants;
+
+public enum Token {
+    OPEN_BRACE("{"),
+    CLOSED_BRACE("}");
+
+    Token(String format) {
+    }
+}
---
+++ src/main/java/jsonparser/ArgumentParser.java
@@ -0,0 +1,20 @@
+package jsonparser;
+
+import java.io.File;
+
+public class ArgumentParser {
+    private ArgumentParser() {}
+    public static File parse(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Error: expected exactly one argument.");
+        }
+
+        File inputFile = new File(args[0]);
+
+        if (inputFile.exists() && inputFile.isFile()) {
+            return inputFile;
+        } else {
+            throw new IllegalArgumentException("Error: input file is invalid or does not exist.");
+        }
+    }
+}
