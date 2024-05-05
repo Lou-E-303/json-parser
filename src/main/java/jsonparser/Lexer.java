5e337501e4b445da28320d27ab8006df9b51ac47
Lou-E-303 <louiscowell404@gmail.com>
2024-05-05 19:23:39 +0100

    Lexer takes files instead of strings

--- src/main/java/jsonparser/Lexer.java
+++ src/main/java/jsonparser/Lexer.java
@@ -2,22 +2,26 @@ package jsonparser;
 
 import constants.Token;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Lexer {
-    public List<Token> lex(String input) {
+    public List<Token> lex(File inputFile) {
         List<Token> tokens = new ArrayList<>();
 
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-
-            for (Token t : Token.values()) {
-                if (t.label == c) {
-                    tokens.add(t);
-                    break;
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            int c;
+            while ((c = reader.read()) > 0) {
+                for (Token t : Token.values()) {
+                    if (t.label == (char) c) {
+                        tokens.add(t);
+                        break;
+                    }
                 }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         return tokens;
