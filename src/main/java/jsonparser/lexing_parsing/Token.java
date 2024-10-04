71ecf6338fd639b0432e640e1341d5eac4811f20
Lou-E-303 <louiscowell404@gmail.com>
2024-10-04 14:43:27 +0100

    Removed String constructor from Token record - no longer required

--- src/main/java/jsonparser/lexing_parsing/Token.java
+++ src/main/java/jsonparser/lexing_parsing/Token.java
@@ -1,11 +1,7 @@
 package jsonparser.lexing_parsing;
 
-public record Token(TokenType type, String content) {
-    public Token(TokenType type, Character value) {
-        this(type, value.toString());
-    }
-
+public record Token(TokenType type, Character value) {
     public Character getValue() {
-        return content.length() == 1 ? content.charAt(0) : null;
+        return value;
     }
 }
