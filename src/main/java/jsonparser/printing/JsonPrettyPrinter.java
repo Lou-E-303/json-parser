b3f6afce955c95a7daf37ffd93132e1556df795e
Lou-E-303 <louiscowell404@gmail.com>
2025-10-15 23:41:43 +0100

    Remove nonsensical test

--- src/main/java/jsonparser/printing/JsonPrettyPrinter.java
+++ src/main/java/jsonparser/printing/JsonPrettyPrinter.java
@@ -1,22 +1,32 @@
 package jsonparser.printing;
 
 import jsonparser.json_objects.Json;
+import jsonparser.json_objects.JsonObject;
 
 import java.util.logging.Logger;
 
 public class JsonPrettyPrinter {
     private static final String INDENT = "  ";
     private final Logger logger = Logger.getLogger(getClass().getName());
-    private int indentLevel = 0;
-    private StringBuilder output = new StringBuilder();
+    private static StringBuilder output = new StringBuilder();
 
     public void print(Json json) {
-        logger.info(getFormattedJsonString(json));
+        logger.info(getFormattedJsonString(json, 0));
     }
 
-    public String getFormattedJsonString(Json json) {
-        Object topLevelValue = json.getValue();
-        output.append(topLevelValue);
+    public String getFormattedJsonString(Json json, int currentIndentLevel) {
+        switch (json) {
+            case JsonObject object : handleJsonObject(object); break;
+            default:
+                throw new IllegalStateException("Unexpected Json type " + json);
+        }
+
+
         return output.toString();
+    }
+
+    private static void handleJsonObject(JsonObject object) {
+        Object topLevelValue = object.getValue();
+        output.append(topLevelValue);
     }
 }
--- src/test/java/printing/JsonPrettyPrinterTest.java
+++ src/test/java/printing/JsonPrettyPrinterTest.java
@@ -2,10 +2,10 @@ package printing;
 
 import jsonparser.exceptions.JsonReadException;
 import jsonparser.json_objects.Json;
-import jsonparser.json_objects.JsonObject;
 import jsonparser.lexing_parsing.JsonLexer;
 import jsonparser.lexing_parsing.JsonParser;
 import jsonparser.printing.JsonPrettyPrinter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,20 +13,21 @@ import java.io.File;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonPrettyPrinterTest {
-    JsonPrettyPrinter printer = new JsonPrettyPrinter();
-    JsonLexer lexer = new JsonLexer();
-    JsonParser parser = new JsonParser();
+    JsonPrettyPrinter printer;
+    JsonLexer lexer;
+    JsonParser parser;
 
-    @Test
-    void givenEmptyJsonThenReturnEmptyString() {
-        Json input = new JsonObject();
-        assertEquals("", printer.getFormattedJsonString(input));
+    @BeforeEach
+    void init() {
+        printer = new JsonPrettyPrinter();
+        lexer = new JsonLexer();
+        parser = new JsonParser();
     }
 
     @Test
     void givenSimpleJsonWithoutIndentationThenReturnCorrectString() throws JsonReadException {
         Json input = parser.parse(lexer.lex(new File("src/test/resources/pass_brackets.json")));
         String expected = "{}";
-        assertEquals(expected, printer.getFormattedJsonString(input));
+        assertEquals(expected, printer.getFormattedJsonString(input, 0));
     }
 }
