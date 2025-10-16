1821f76ae0d59a595ace5f9e575d99ef98eb8770
Lou-E-303 <louiscowell404@gmail.com>
2025-10-16 14:09:19 +0100

    Remove argument to file parser - not needed

--- src/main/java/jsonparser/Main.java
+++ src/main/java/jsonparser/Main.java
@@ -1,27 +1,7 @@
 package jsonparser;
 
-import jsonparser.json_objects.Json;
-import jsonparser.lexing_parsing.ArgumentToFileParser;
-import jsonparser.lexing_parsing.JsonParser;
-import jsonparser.lexing_parsing.JsonLexer;
-import jsonparser.lexing_parsing.Token;
-
-import java.io.File;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            JsonParser parser = new JsonParser();
-            File inputFile = ArgumentToFileParser.parse(args);
-            List<Token> tokens = new JsonLexer().lex(inputFile);
-            Json json = parser.parse(tokens);
-            parser.reset();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.exit(1);
-        }
-
-        System.exit(0);
+        // TODO delete this?
     }
 }
